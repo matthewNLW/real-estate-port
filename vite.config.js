da31291ab2b41c3d3497e1ce0419e680b4becf82
Matthew Harrison <matthew@newleafweb.com>
2026-01-19 13:31:23 -0500

    netlify

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   build: {
+    outDir: 'dist',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        about: resolve(__dirname, 'about.html'),
-        services: resolve(__dirname, 'services.html'),
-        contact: resolve(__dirname, 'contact.html'),
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+        developments: resolve(__dirname, "developments.html"),
+        developmentDetail: resolve(__dirname, "development-detail.html"),
+        contact: resolve(__dirname, "contact.html"),
       },
     },
   },
